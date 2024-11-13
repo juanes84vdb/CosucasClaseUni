5a3dd92503044e5d917fb5e56b94387752f7c110
juanes84vdb <mariovj222003.mv2@gmail.com>
2024-11-13 09:45:16 +0100

    ejer 4 matrices, sin puntos

--- practica/Progra/clase/Prueba/main.c
+++ practica/Progra/clase/Prueba/main.c
@@ -3,25 +3,39 @@
 #include <string.h>
 #include <math.h>
 
-int fibonacci(int);
+#define N 10
 
 int main(int argc, char *argv[])
 {
-    int fib = 1;
-    int n;
+    char matriz[N][N];
+    char texto[N + 1];
 
-    scanf("%d", &n);
-    fib = fibonacci(n);
-    printf("fib(%d) = %d\n", n, fib);
-    return 0;
-}
+    printf("Ingrese el texto: ");
+    gets(texto);
 
-int fibonacci(int n)
-{
-    if (n == 1)
-        return 1;
-    else if (n == 0)
+    for (int i = 0; i <= N; i++)
+    {
+        matriz[i][i] = texto[i];
+        for (int j = i + 1; j <= N; j++)
+        {
+            matriz[i][j] = matriz[i][j - 1] + 1;
+            if (matriz[i][j] == 123)
+                matriz[i][j] = 97;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                matriz[i][j] = matriz[i][j + 1] - 1;
+                if (matriz[i][j] == 96)
+                    matriz[i][j] = 122;
+            }
+        }
+    }
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                printf("%c ", matriz[i][j]);
+            }
+            printf("\n");
+        }
         return 0;
-    else
-        return fibonacci (n - 1) + fibonacci(n-2) ;
-}
+    }
